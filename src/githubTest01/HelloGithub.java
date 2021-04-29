572600c274bbfcde8579b150fe3095206391e6eb
max-xiang <bigdaxiang@163.com>
2021-04-29 23:29:14 +0800

    this is a test java push github

--- src/githubTest01/HelloGithub.java
+++ src/githubTest01/HelloGithub.java
@@ -4,6 +4,6 @@ public class HelloGithub {
     public static void main(String[] args) {
         System.out.println("hello github i am try share my code on you sever thank you!");
         System.out.println("更新一下！");
-        System.out.println("远程仓库更改名后我这面也要改链接");
+
     }
 }
