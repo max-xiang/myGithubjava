33aa0cac9d33cb154fcb508fee4121406bf16bc0
max-xiang <bigdaxiang@163.com>
2021-04-29 16:32:43 +0800

    this is a fist preject

---
+++ src/githubTest01/HelloGithub.java
@@ -0,0 +1,7 @@
+package githubTest01;
+
+public class HelloGithub {
+    public static void main(String[] args) {
+        System.out.println("hello github i am try share my code on you sever thank you!");
+    }
+}
