142066a5ae7c9b4b2799455d71be8627674601d4
max-xiang <bigdaxiang@163.com>
2021-04-29 23:21:31 +0800

    this is a test java push github

--- src/githubTest01/HelloGithub.java
+++ src/githubTest01/HelloGithub.java
@@ -3,5 +3,6 @@ package githubTest01;
 public class HelloGithub {
     public static void main(String[] args) {
         System.out.println("hello github i am try share my code on you sever thank you!");
+        System.out.println("更新一下！");
     }
 }
